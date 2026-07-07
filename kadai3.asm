bcf1b5a989ec36a9bd840ac5e7d1c6599294427c
setsugan <setsugan.robocon@gmail.com>
2026-07-07 13:05:43 +0900

    feat: 整理

--- kadai3.asm
+++ kadai3.asm
@@ -3,11 +3,11 @@ STATUS  EQU     3
 RP0     EQU     5
 PORTB   EQU     6
 TRISB   EQU     H'86'
-        CLRF    PORTB       ; LEDを全部消す
-        BSF     STATUS,RP0  ; バンク1に切り替える
-        CLRF    TRISB       ; PORTBを全部出力にする
-        BCF     STATUS,RP0  ; バンク0に戻す
-        MOVLW   B'00111111' ; 0表示なので0,1,2,3,4,5番目を点灯
-        MOVWF   PORTB       ; PORTBへ転送して7segに0を出す
-LOOP    GOTO    LOOP        ; そのまま表示し続ける
+        CLRF    PORTB       ; ledをぜんぶけしとく
+        BSF     STATUS,RP0  ; ぺーじを１にする
+        CLRF    TRISB       ; ledをぜんぶ出力モードにする
+        BCF     STATUS,RP0  ; ぺーじを０にする
+        MOVLW   B'00111111' ; 0表示
+        MOVWF   PORTB       ; PORTBへ転送
+LOOP    GOTO    LOOP        ; くるくる
         END
