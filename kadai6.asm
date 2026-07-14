9431f84de51d97f5089272fbea4fdfd682ca827c
setsugan <setsugan.robocon@gmail.com>
2026-07-14 14:22:21 +0900

    feat: うごいた！！！感謝

--- kadai6.asm
+++ kadai6.asm
@@ -1,9 +1,12 @@
-        LIST    P=16F84A,R=DEC
-STATUS  EQU     3
-RP0     EQU     5
-PORTB   EQU     6
-TRISB   EQU     H'86'
-        CLRF    PORTB       ; ledをぜんぶけしとく
-        BSF     STATUS,RP0  ; ぺーじを１にする
-        CLRF    TRISB       ; PORTBを全部出力にする
-        BCF     STATUS,RP0  ; ぺーじを０にする
+                LIST    P=16F84A,R=DEC
+STATUS          EQU     3
+RP0             EQU     5
+PORTB           EQU     6
+TRISB           EQU     H'86'
+COUNTER1        EQU     H'10'
+COUNTER2        EQU     H'11'
+                CLRF    PORTB           ; ledをぜんぶけしとく
+                BSF     STATUS,RP0      ; ぺーじを１にする
+                CLRF    TRISB           ; PORTBを全部出力にする
+                BCF     STATUS,RP0      ; ぺーじを０にする
+MAIN            BSF     PORTB           ; 
